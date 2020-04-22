197157a45a1be108dce5fb9ff364abbc16bb0ef4
PARTHA PRATIM SAHU <sahurahulkumar5@gmail.com>
2020-04-22 15:17:18 +0530

    Update variablesizedarray.cpp

--- variablesizedarray.cpp
+++ variablesizedarray.cpp
@@ -6,19 +6,23 @@
 using namespace std;
 
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    int n, q; cin >> n >> q;
+int main() 
+{
+    int n, q;
+    cin >> n >> q;
     int **v = new int*[n];
-    for(int j=0;j<n;j++){
+    for(int j=0;j<n;j++)
+    {
         int k; cin >> k;
         v[j] = new int[k];
-        for(int i=0;i<k;i++){
+        for(int i=0;i<k;i++)
+        {
             int x; cin >> x;
             v[j][i] = x;
         }
     }
-    for(int i=0;i<q;i++){
+    for(int i=0;i<q;i++)
+    {
         int a, b; cin >> a >> b;
         cout << v[a][b]<<endl;
     }
